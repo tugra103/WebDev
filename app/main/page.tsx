993f7f320477e4a9d077faef8ee87be6d3c26de9
tugra103 <tugrabulbul2020@gmail.com>
2026-04-02 18:12:56 +0300

    .

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -87,7 +87,7 @@ export default function TemplateDemo() {
 
     return (
         <div className="card">
-            <Menubar model={items} start={start} end={end} className="shadow-md rounded-md h-12" />
+            <Menubar model={items} start={start} end={end} className="shadow-md rounded-md h-12 gap-2" />
         </div>
     )
 }
