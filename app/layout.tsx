929d124a4c5fb77ff764546f207acad77395eb2b
tugra103 <tugrabulbul2020@gmail.com>
2026-04-03 08:56:27 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { FingerprintProvider } from '@fingerprint/react'
+import { AuthProvider } from "@/context/AuthContext";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,19 +38,6 @@ export default function RootLayout({
         </FingerprintProvider>
         </AuthProvider>
         </body>
-    </html>
-  );
-}
-import { AuthProvider } from "@/context/AuthContext";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html>
-      <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </body>
     </html>
   );
 }
